b0fb1f8db402b9b12091b1aa300e394541b28dd5
Dmytro-Nepelyak <dimanepelyak@tntu.edu.ua>
2025-03-20 01:13:33 +0200

    Завдання 1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
-//Дано речення, слова в якому розділені пропусками (одним або декількома).
-// Змінити кожне слово в рядку, вилучивши з нього всі наступні
-// входження першої букви цього слова (кількість пропусків між словами не змінювати).
-
 public class Main {
     public static void main(String[] args) {
+        String sentence = "";
+        for (String arg : args) {
+            sentence += arg + " ";
+        }
+        System.out.println("Речення: " + sentence);
+
+        String consonants = "бвгґджзйклмнпрстфхцчшщБВГҐДЖЗЙКЛМНПРСТФХЦЧШЩ";
+
+        String result = "";
+
+        for (char c : sentence.toCharArray()) {
+            if (consonants.indexOf(c) != -1) {
+                if (Character.isUpperCase(c)) {
+                    result += Character.toString(Character.toLowerCase(c));
+                } else {
+                    result += Character.toString(Character.toUpperCase(c));
+                }
+            } else {
+                result += Character.toString(c);
+            }
+        }
+        System.out.println("Результат: " + result);
+
+        System.out.println("Завдання №2: ");
+        zavd2();
+    }
+
+    public static void zavd2() {
         String sentence = "Як умру, то    поховайте Мене на могилі,  Серед степу широкого, На   Вкраїні милій";
         String[] words = sentence.split(" ");
         String newSentence = "";
         for (String word : words) {
             if (word.isEmpty()) {
-              newSentence = newSentence.concat(" ");
+                newSentence = newSentence.concat(" ");
                 continue;
             }
             word = word.charAt(0) + word.replaceAll(String.valueOf(word.charAt(0)), "");
@@ -18,3 +42,4 @@ public class Main {
         System.out.println(newSentence);
     }
 }
+
